74f498f07ee4fb2a5d0acf9bc16bdd36c31d3005
root <root@chaoshost.chaos>
2013-12-04 20:51:52 +0100

    Add network::ifcfg custom type to create ifcfg-* files.
    It works, but the ifcfg.erb template creates really ugly
    files.

---
+++ manifests/ifcfg.pp
@@ -0,0 +1,52 @@
+# manage RedHat style ifcfg-* interface configuration files
+define network::ifcfg ( $bonding_opts = undef,
+                        $bootproto = "dhcp",
+                        $broadcast = undef,
+                        $device = $title,
+                        $dhcp_hostname = undef,
+                        $dhcpv6c = undef,
+                        $dhcpv6c_options = undef,
+                        $dns1 = undef,
+                        $dns2 = undef,
+                        $ethtool_opts = undef,
+                        $hotplug = undef,
+                        $hwaddr = undef,
+                        $ipaddr = undef,
+                        $ipv6addr = undef,
+                        $ipv6addr_secondaries = undef,
+                        $linkdelay = undef,
+                        $macaddr = undef,
+                        $master = undef,
+                        $netmask = undef,
+                        $network = undef,
+                        $nm_controlled = false,
+                        $onboot = true,
+                        $peerdns = undef,
+                        $slave = undef,
+                        $srcaddr = undef,
+                        $userctl = undef,
+                        $scope = undef,
+                        $uuid = undef,
+                        $ensure = undef,
+                        $type = "Ethernet",
+                        $gateway = undef,
+                        $ipv6init = undef ){
+
+  if $hwaddr {
+    $ifhwaddr = $hwaddr
+  }
+  else {
+    $ifhwaddr = getvar("::macaddress_${title}")
+  }
+
+  file { "/etc/sysconfig/network-scripts/ifcfg-${title}":
+    ensure => $ensure ? {
+      absent => absent,
+      default => present
+    },
+    owner => root,
+    group => root,
+    mode => 0644,
+    content => template("$module_name/ifcfg.erb")
+  }
+}
