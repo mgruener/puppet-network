7ab23c4a52678de0bb68af49e221fefa3aa4f9df
root <root@chaoshost.chaos>
2013-12-04 21:08:19 +0100

    - add default value "present" for ensure parameter of ifcfg type - core class: use create resource to create all supplied network interfaces   chose the resource type based on the osfamily (only RedHat   is supported so far) - require net-tools on RedHat systems

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,11 @@
-class network {
+class network ( $interfaces = hiera_hash("interfaces")) {
+  case $osfamily {
+    'RedHat': { package { "net-tools": }
+                $resource = "${module_name}::ifcfg"
+    }
+  }
+
+  if $interfaces {
+    create_resources($resource,$interfaces)
+  }
 }
