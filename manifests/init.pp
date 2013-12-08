0631a1735c9ad33014ea70d8473ca447789924c2
Michael Gruener <michael.gruener@chaosmoon.net>
2013-12-08 01:37:53 +0100

    Manage network services

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,49 @@
-class network ( $interfaces = hiera_hash("${module_name}::interfaces",undef)) {
+class network ( $interfaces = hiera_hash("${module_name}::interfaces",undef),
+                $networkmanager = hiera("${module_name}::networkmanager",false)) {
+
+  validate_bool($networkmanager)
+
   case $osfamily {
     'RedHat': { package { "net-tools": }
                 $resource = "${module_name}::ifcfg"
     }
   }
 
+  case $::operatingsystem {
+    'Fedora': { $servicename = 'NetworkManager.service'
+                $serviceprovider = systemd
+    }
+    default: {  $servicename = 'NetworkManager'
+                $serviceprovider = undef
+    }
+  }
+
+  if $networkmanager {
+    service { $servicename:
+      ensure   => running,
+      enable   => true,
+      provider => $serviceprovider,
+    }
+
+    service { 'network':
+      ensure => stopped,
+      enable => false,
+    }
+  }
+  else {
+    service { $servicename:
+      ensure   => stopped,
+      enable   => false,
+      provider => $serviceprovider,
+    }
+
+    service { 'network':
+      ensure => running,
+      enable => true,
+    }
+  }
+
   if $interfaces {
-    create_resources($resource,$interfaces)
+    create_resources($resource,$interfaces, require => [ Service[$servicename], Service['network'] ])
   }
 }
