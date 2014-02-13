c9d6f7ab66187ee07f08c18d89716d27dec57ab3
Michael Gruener <michael.gruener@chaosmoon.net>
2014-02-13 14:33:37 +0100

    Use the hiera_merge pattern proposed by ghoneycutt as a ("not very nice") workaround for https://tickets.puppetlabs.com/browse/HI-118

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,36 @@
-class network ( 
-  $interfaces = hiera_hash("${module_name}::interfaces",undef),
-  $networkmanager = hiera("${module_name}::networkmanager",false)
+class network (
+  $interfaces = undef,
+  $networkmanager = false,
+  $hiera_merge = false,
 ) {
 
-  validate_bool($networkmanager)
+  $myclass = $module_name
+
+  case type($hiera_merge) {
+    'string': {
+      validate_re($hiera_merge, '^(true|false)$', "${myclass}::hiera_merge may be either 'true' or 'false' and is set to <${hiera_merge}>.")
+      $hiera_merge_real = str2bool($hiera_merge)
+    }
+    'boolean': {
+      $hiera_merge_real = $hiera_merge
+    }
+    default: {
+      fail("${myclass}::hiera_merge type must be true or false.")
+    }
+  }
+
+  case type($networkmanager) {
+    'string': {
+      validate_re($networkmanager, '^(true|false)$', "${myclass}::networkmanager may be either 'true' or 'false' and is set to <${networkmanager}>.")
+      $networkmanager_real = str2bool($networkmanager)
+    }
+    'boolean': {
+      $networkmanager_real = $networkmanager
+    }
+    default: {
+      fail("${myclass}::networkmanager type must be true or false.")
+    }
+  }
 
   case $::osfamily {
     'RedHat': { package { 'net-tools': }
@@ -21,7 +48,7 @@ class network (
     }
   }
 
-  if $networkmanager {
+  if $networkmanager_real {
     service { $servicename:
       ensure   => running,
       enable   => true,
@@ -46,7 +73,17 @@ class network (
     }
   }
 
-  if $interfaces {
-    create_resources($resource,$interfaces, { require => [ Service[$servicename], Service['network']] })
+  if $interfaces != undef {
+    if !is_hash($interfaces) {
+        fail("${myclass}::interfaces must be a hash.")
+    }
+
+    if $hiera_merge_real == true {
+      $interfaces_real = hiera_hash("${myclass}::interfaces",undef)
+    } else {
+      $interfaces_real = $interfaces
+    }
+
+    create_resources($resource,$interfaces_real, { require => [ Service[$servicename], Service['network']] })
   }
 }
